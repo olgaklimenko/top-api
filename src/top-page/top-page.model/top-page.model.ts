08c3519762c06dbae786efb3874ee23761e6a903
Olga Klimenko <oklimenko92@mail.ru>
2024-08-09 13:04:59 +0700

    add modules with models

---
+++ src/top-page/top-page.model/top-page.model.ts
@@ -0,0 +1,26 @@
+export enum TopLevelCategory {
+	Cources,
+	Services,
+	Books,
+	Products
+}
+
+export class TopPageModel {
+	firstCategory: TopLevelCategory;
+	secondCategory: string;
+	title: string;
+	category: string;
+	hh?: {
+		count: number;
+		juniorSalary: number;
+		middleSalary: number;
+		seniorSalary: number;
+	};
+	advantages: {
+		title: string;
+		description: string;
+	}[];
+	seoTest: string;
+	tagsTitle: string;
+	tags: string[];
+}
